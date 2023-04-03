9458ba9b708db293f88acd234765930ee49986fd
lPedru <dejesus.pedro2002@gmail.com>
2023-04-03 09:05:38 -0300

    stuff

---
+++ src/abstract-factory/medium-chair.ts
@@ -0,0 +1,12 @@
+
+import { Chair } from './chair'
+
+export default class MediumChair extends Chair {
+    constructor() {
+        super()
+        this.name = 'MediumChair'
+        this.height = 60
+        this.width = 60
+        this.depth = 60
+    }
+}
---
+++ src/factory/big-chair.ts
@@ -0,0 +1,11 @@
+
+import Chair from './chair'
+
+export default class BigChair extends Chair {
+    constructor() {
+        super()
+        this.height = 80
+        this.width = 80
+        this.depth = 80
+    }
+}
